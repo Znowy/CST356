66d30c0f9db655c601276469bd96defba88f21b2
Znowy <jknudsen99@gmail.com>
2020-02-01 11:27:20 -0800

    Completed lab 4

---
+++ Week4/Exercises/src/js/students.js
@@ -0,0 +1,22 @@
+var students = '{"students": [{"studentId": 12345, "email": "jake.student@oit.edu"}, {"studentId": 23456, "email": "david.student@oit.edu"}, {"studentId": 34567, "email": "brad.student@oit.edu"}, {"studentId": 45678, "email": "andrew.student@oit.edu"}, {"studentId": 56789, "email": "nick.student@oit.edu"}]}';
+
+function getStudentParse()
+{
+    return JSON.parse(students);
+}
+
+function getStudents()
+{
+    var data = getStudentParse();
+    displayStudents(data.students);
+}
+
+function displayStudents(students)
+{
+    var tableData = "";
+    for (i=0; i < students.length; i++)
+    {
+        tableData += "<tr><td>" + students[i].studentId + "</td><td>" + students[i].email + "</td>";
+        document.getElementById("studentList").innerHTML = tableData;
+    }
+}
